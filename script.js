dc560eafd4074f60713d82eaab040a0ad1d733df
bloxain <100310884+bloxain@users.noreply.github.com>
2024-04-24 12:30:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 /// execute_script.js
 window.addEventListener("keyup", event => {
-    if (event.ctrlKey && event.which === 192) {
+    if (event.ctrlKey && !event.altKey && !event.shiftKey && !event.metaKey && event.which === 192) {
         let els = Array.from(document.querySelectorAll("head > style"))
             .filter(el => el.innerText.match(/{\s*display\s*:\s*none\s+!important\s*;?\s*}/i));
         els.forEach(el => el.innerText = "");
         if (els.length > 0) alert(`finished removing the ${els.length} youtube restructions boss! luphoria would be proud.`);
         else alert("nothing found :(");
+    }
+    if (!window.eruda && event.ctrlKey && !event.altKey && event.shiftKey && !event.metaKey && event.which === 192) {
+        var script = document.createElement('script');
+        script.src = "https://cdn.jsdelivr.net/npm/eruda";
+        script.onload = function () { eruda.init(); alert("eruda loaded"); };
+        document.body.appendChild(script);
     }
 });
