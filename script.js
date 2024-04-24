99ca82200be9a9e2ca129381ca9670e88c1c5709
bloxain <100310884+bloxain@users.noreply.github.com>
2024-04-24 10:12:37 -0700

    updated

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 /// execute_script.js
-window.setInterval(() => {
-    let els = Array.from(document.querySelectorAll("head > style"))
-        .filter(el => el.innerText.match(/{s*displays*:s*nones+!importants*;?s*}/i));
-    els.forEach(el => el.innerText = "");
-    if (els.length > 0) alert(`finished removing the ${els.length} youtube restructions boss! luphoria would be proud.`);
-}, 500);
+window.addEventListener("keyup", event => {
+    if (event.ctrlKey && event.which === 192) {
+        let els = Array.from(document.querySelectorAll("head > style"))
+            .filter(el => el.innerText.match(/{\s*display\s*:\s*none\s+!important\s*;?\s*}/i));
+        els.forEach(el => el.innerText = "");
+        if (els.length > 0) alert(`finished removing the ${els.length} youtube restructions boss! luphoria would be proud.`);
+        else alert("nothing found :(");
+    }
+});
